2a996462d2c162eab5b40a119994255cb616c94a
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2023-07-18 16:18:32 +0530

    Setup basic client infrastucutre

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+function App() {
+ 
+
+  return (
+    <div className='app'>App</div>
+  )
+}
+
+export default App
